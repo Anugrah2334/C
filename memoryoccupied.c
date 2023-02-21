02b6b6ca443f1997d4b5ca0d22cab2ea5e5907b4
Anugrah <117362551+Anugrah2334@users.noreply.github.com>
2023-02-21 17:30:22 +0530

    Memory Occupied

---
+++ memoryoccupied.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+void main()
+{
+    int x;
+    float y;
+    char z;
+    double a;
+    printf("\n To check the memory occupied by different data types:");
+    printf("\n The memory occupies by int data type=%d\n",sizeof(x));
+    printf("\n The memory occupies by float data type=%d\n",sizeof(y));
+    printf("\n The memory occupies by char data type=%d\n",sizeof(z));
+    printf("\n The memory occupies by double data type=%d\n",sizeof(a));
+    printf("\n Hope you understood!");
+}
